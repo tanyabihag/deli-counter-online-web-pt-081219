b82f71c6635166ff1d31c78b513c46d94e21cc0a
Tanya Bihag <tbihag@gmail.com>
2019-08-21 21:48:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,6 @@ other_deli = ["Logan", "Avi", "Spencer"]
 another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 def line(katz_deli) 
-  while line == ""
    puts "The line is currently empty"
 end  
 
