8d6526aca63b89ec8fef92ddc0e30b6a8f4886cc
Tanya Bihag <tbihag@gmail.com>
2019-08-21 21:35:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,16 @@
 katz_deli = []
 other_deli = ["Logan", "Avi", "Spencer"]
-another_deli = [""]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 def line(katz_deli)
   while line == ""
    puts "The line is currently empty"
 end  
+
+def line(other_deli)
+  while line == "other_deli"
+    puts "This line is currently #{other_deli}."
+end
 
 def take_a_number(katz_deli)
   katz_deli = []
